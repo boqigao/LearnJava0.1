ea85c9eae792e2284155a20e65e70ac8914930e2
boqigaoosakauniversity <gaoboqi@gmail.com>
2020-01-07 11:29:49 +0900

    ver0.3

--- src/cn/boqi/game/MyGameFrame.java
+++ src/cn/boqi/game/MyGameFrame.java
@@ -14,14 +14,30 @@ public class MyGameFrame extends JFrame {
     Image plane = GameUtil.getImage("images/plane.png");
     Image bg = GameUtil.getImage("images/bg.png");
 
+    int x = 250;
+    int y = 250;
+
     @Override
     public void paint(Graphics g){
         g.drawImage(bg,0,0,null);
-        g.drawImage(plane, 250,250,null);
-
+        g.drawImage(plane, x, y,null);
+        x++;
     }
 
-    class PaintThread{
+    //帮助我们反复的重画窗口！
+    class PaintThread extends Thread{
+        @Override
+        public void run(){
+            while(true){
+                repaint();  //重画
+
+                try {
+                    Thread.sleep(20); //1秒刷新50次
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
@@ -37,6 +53,8 @@ public class MyGameFrame extends JFrame {
                 System.exit(0);
             }
         });
+
+        new PaintThread().start();  //启动重画窗口的线程
     }
 
     public static void main(String[] args) {
